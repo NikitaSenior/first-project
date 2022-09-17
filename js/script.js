4d5d3942ad5b1a27a36f760b1619c0f37230c910
Nikita <bustuiseniy@gmail.com>
2022-09-17 13:14:49 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+"use strict";
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a: 50
+// };
+
+// number = obj.a;
+
+// console.log(obj["a"]);
+
+
+// let arr = ['erer', 'fdgfgddddd', 6, 'eeeeeee', {}, []];
+
+// console.log(arr);
+
+
+// const obj2 = {
+//     0: 50,
+//     3: 30
+// };
+// console.log(obj2['3']);
+
+// const result = confirm("dfgdfdfgdfg");
+// const anwer = prompt("Are youe 18?", "18");
+// console.log(anwer);
+
+const anwers = [];
+anwers[0] = prompt('name', '');
+anwers[0] = prompt('age', '');
+let number = 5;
+console.log(number);
